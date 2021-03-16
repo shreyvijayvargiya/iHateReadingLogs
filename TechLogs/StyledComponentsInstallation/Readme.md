1493c3b93fdfdc598144a0afe97e7feca0212086
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2021-03-16 15:56:33 +0530

    article added and repo seup completed

---
+++ TechLogs/StyledComponentsInstallation/Readme.md
@@ -0,0 +1,6 @@
+#Perfect styling library in react
+
+##This repo gives basic understanding of perfect styling library in react.
+
+Read the article for better understanding 
+### https://shreyvijayvargiya26.medium.com/perfect-styling-library-for-react-933c533bd1f4
---
+++ TechLogs/StyledComponentsInstallation/pages/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Button, TextField } from '@material-ui/core';
+import styled from 'styled-components';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello, Welcome to styled-components installlation</h1>
+            <LoginContainer bgColor="#EEEEEE">
+                <TextField  
+                    name="name"
+                    fullWidth
+                    size="small"
+                    placeholder="Enter the name"
+                    variant="outlined"
+                    color="secondary"
+                />
+                <br />
+                <br />
+                <CustomButton variant="contained" color="primary" size="large">Hello</CustomButton>
+            </LoginContainer>
+        </div>
+    )
+};
+export default App;
+
+const LoginContainer = styled.div`
+    width: 50%;
+    margin: auto;
+    padding: 10px;
+    text-align: center;
+    border: 1px solid #EEEEEE;
+    border-radius: 6px;
+    background-color: ${props => props.bgColor };
+`;
+
+const CustomButton = styled(Button)`
+    width: 80%;
+`;
+
+
