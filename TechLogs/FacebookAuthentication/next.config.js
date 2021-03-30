96832d238f427516ee13daaf3ec0e9ed7cfa0e8b
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2021-03-31 00:30:10 +0530

    facebook authenticatio completed, article added

---
+++ TechLogs/FacebookAuthentication/next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    env: {
+        apiKey: "AIzaSyB8AfsM9H5DE_kzNKwbB2YNAoqgihw7OLk",
+        authDomain: "techlogs-756dd.firebaseapp.com",
+        databaseUrl: "https://techlogs-756dd-default-rtdb.europe-west1.firebasedatabase.app/",
+        projectId: "techlogs-756dd",
+        storageBucket: "techlogs-756dd.appspot.com",
+        messagingSenderId: "600694053578",
+        appId: "1:600694053578:web:6ba3fecad446c653293233",
+        measurementId: "G-36TNP6MKTX"
+    }
+}
