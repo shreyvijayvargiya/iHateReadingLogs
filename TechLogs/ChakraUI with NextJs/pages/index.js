ef9332766de1d4a3e6aed4e95ed66bd904437552
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2021-05-26 16:39:21 +0530

    custom tabs project completed

--- TechLogs/ChakraUI with NextJs/pages/index.js
+++ TechLogs/ChakraUI with NextJs/pages/index.js
@@ -1,18 +1,63 @@
-import React from 'react';
-import { Button, Flex, Container, Heading, Box, Link, Image, Divider }  from '@chakra-ui/react';
-
+import React, { useEffect } from 'react';
+import { Button, Flex, Container, Heading, Box, Link, Image, Text }  from '@chakra-ui/react';
+import axios from 'axios';
+import { useRouter } from 'next/router';
 
 const App = () => {
+
+    const [ data, setData] = React.useState(null);
+    const [ loading, setLoading ] = React.useState(true);
+    const router = useRouter();
+    
+
+ 
+    async function fetchData(mounted, source){
+        try {
+            if(mounted){
+                setLoading(false)
+            }
+            const response = await axios.get("https://jsonplaceholder.typicode.com/todos", { cancelToken: source.token });
+            setData(response.data);
+        }catch(e){
+            if(axios.isCancel(e)){
+                
+            }else {
+                throw e
+            }
+        }
+    };
+
+    React.useEffect(() => {
+        const source = axios.CancelToken.source();
+        let mounted = true;
+        // fetchData(mounted, source);
+        return () => {
+            mounted = false
+            alert("Done")
+            source.cancel()
+        }
+    }, [ ]);
+
+
+
     return (
         <Container padding="10" maxW="100%" textAlign="center">
             <Heading>Chakra UI with Next JS</Heading>
+            <Box>
+                <Button variant="outlined" padding="2" background="blue.500" size="large"
+                    onClick={() => setLoading({ loader: true }, () => console.log(loading, 'loading'))}
+                >
+                    About
+                </Button>
+            </Box>
             <Box borderBottomWidth="1px" borderColor="black" padding="2" marginTop="4">
                 <Button variant="outlined" padding="2" background="green.500" size="large">
-                    <a>
+                    <a href="https://shreyvijayvargiya26.medium.com/next-js-with-chakra-ui-testing-the-claims-of-being-the-best-ui-library-fe4bec82c4cd">
                         Read the instructions
                     </a>
                 </Button>
             </Box>
+            {loading && <Text>Loading data...</Text>}
             <Flex alignItems="center" justify="space-around">
                 <Box bg="blue.200" padding="20" margin="1" shadow="xl" borderRadius="10">
                     <Heading size="xl">Logs Panel</Heading>
@@ -43,7 +88,7 @@ const App = () => {
                         alt="Creativity"
                     />
                     <Box borderBottomWidth="1px" borderTopWidth="1px" borderColor="black" padding="2" marginTop="10">
-                        <Button variant="outlined" padding="2" background="blue.500" size="large">
+                        <Button variant="outlined" padding="2" background="yellow.500" size="large">
                             <a href="https://www.ihatereading.in/creativity">
                                 View Creativity
                             </a>
@@ -61,7 +106,7 @@ const App = () => {
                         alt="Projects"
                     />
                     <Box borderBottomWidth="1px" borderTopWidth="1px" borderColor="black" padding="2" marginTop="10">
-                        <Button variant="outlined" padding="2" background="blue.500" size="large">
+                        <Button variant="outlined" padding="2" background="green.500" size="large">
                             <a href="https://www.ihatereading.in/projects">
                                 Show Projects
                             </a>
---
+++ TechLogsMobile/ReactNativeWithFirebase/App.js
@@ -0,0 +1,108 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import database from '@react-native-firebase/database';
+import {
+  Dimensions,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+const Section = ({children, title}) => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  
+  React.useEffect(() => {
+    database()
+    .ref('/users')
+    .on('value', (snap, error) =>{
+      if(error){
+        console.log(error, 'error');
+        return
+      }
+      console.log(snap.val())
+    })
+
+  },[ ]);
+
+  return (
+    <SafeAreaView style={styles.container}>
+     <View>
+       <Text>Firebase installation in React</Text>
+     </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  container: {
+    width: Dimensions.get("screen").width,
+    height: Dimensions.get("screen").width,
+    textAlign: 'center',
+    padding: 20
+  },
+
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
