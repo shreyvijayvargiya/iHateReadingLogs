de5f754878f72bc31f60d72384b1af2f84fce0aa
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2021-11-24 19:23:56 +0530

    Intial commit by simplegit

--- routes/routes.js
+++ routes/routes.js
@@ -53,7 +53,41 @@ router.get('/auth/google/callback', passport.authenticate('google', { failureRed
 
 
 router.get('/push-repo', (req, res) => {
-   
+    shellJs.cd(process.cwd() + '/repos/root');
+    const repo = 'Dummy';
+    const username = 'shreyvijayvargiya';
+    const password = 'Treyvijay26';
+    const githubUrl = 'https://github.com/shreykoo/ihatereading-repos.git';
+
+    gitSimple.addConfig('user.email','shreyvijayvargiya26@gmail.com');
+    gitSimple.addConfig('user.name', username);
+    
+    // Add remore repo url as origin to repo
+    gitSimple.init().addRemote('origin', 'https://github.com/shreykoo/ihatereading-repos.git');
+    // Add all files for commit
+    gitSimple.add('.')
+        .then(
+           (addSuccess) => {
+              console.log(addSuccess);
+           }, (failedAdd) => {
+              console.log('adding files failed');
+        });
+    // Commit files as Initial Commit
+    gitSimple.commit('Intial commit by simplegit')
+       .then(
+          (successCommit) => {
+            console.log(successCommit);
+         }, (failed) => {
+            console.log('failed commmit');
+     });
+    // Finally push to online repository
+    gitSimple.push('origin','master')
+        .then((success) => {
+           console.log('repo successfully pushed');
+        },(failed)=> {
+            console.log(failed)
+           console.log('repo push failed');
+    });
 })
 module.exports = router;
 
