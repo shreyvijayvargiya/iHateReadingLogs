d527fd054d57eb00a6021cc7d045895583918859
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2021-11-24 19:35:03 +0530

    Intial commit by simplegit

--- routes/routes.js
+++ routes/routes.js
@@ -53,8 +53,7 @@ router.get('/auth/google/callback', passport.authenticate('google', { failureRed
 
 
 router.get('/push-repo', (req, res) => {
-    shellJs.cd(process.cwd() + '/repos/root');
-    console.log(process.cwd(), 'process.cwd()');
+    shellJs.cd(process.cwd());
     const repo = 'Dummy';
     const username = 'shreyvijayvargiya';
     const password = 'Treyvijay26';
